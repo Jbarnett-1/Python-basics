54917568c49926e9be9cb129ecdea93b2955ebe7
Jerrika Barnett <jerrikabarnett@gmail.com>
2020-07-16 20:34:49 -0500

    Create list and order.py

---
+++ list and order.py
@@ -0,0 +1,19 @@
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0]) # list only first bicycle
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[0].title()) # capitalize first bicycle 
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[1])
+print(bicycles[3])
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles[-1]) # access the last item of a list
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+message = f"My first bicycle was a {bicycles[0].title()}."
+print(message)
